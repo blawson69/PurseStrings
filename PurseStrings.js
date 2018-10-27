ee45f03a55a720fb08350cb5b881a299a27b0c7d
Ben L <39037350+blawson69@users.noreply.github.com>
2018-10-27 13:52:03 -0700

    Corrected version number

--- PurseStrings.js
+++ PurseStrings.js
@@ -12,7 +12,7 @@ var PurseStrings = PurseStrings || (function () {
 
     //---- INFO ----//
 
-    var version = '2.1',
+    var version = '2.2',
 		attributes = {cp:'pursestrings_cp',sp:'pursestrings_sp',ep:'pursestrings_ep',gp:'pursestrings_gp',pp:'pursestrings_pp'},
 		dropChange = false,
 
